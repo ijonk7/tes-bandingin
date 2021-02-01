7672f5b1877880f2dbc824a391805c7f6f03d768
ijonk7 <jookeo.rizal@gmail.com>
2021-02-02 01:11:38 +0700

    create login page

---
+++ routes/web.php
@@ -0,0 +1,28 @@
+<?php
+
+use App\Http\Controllers\Auth\LoginController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Auth::routes([
+    'login'    => true,
+    'logout'   => true,
+    'register' => false,
+    'reset'    => false,   // for resetting passwords
+    'confirm'  => false,  // for additional password confirmations
+    'verify'   => false,  // for email verification
+]);
+
+Route::get('/', [LoginController::class, 'showLoginForm']);
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
